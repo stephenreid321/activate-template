182977c97c5d144a8a04af01ce5e937117b6fc3a
Stephen Reid <postscript07@gmail.com>
2013-06-18 00:03:33 +0100

    .

--- app/controllers/blog.rb
+++ app/controllers/blog.rb
@@ -2,11 +2,13 @@ ActivateApp::App.controllers :blog do
   
   get :index do
     @blog_posts = BlogPost.order_by(:created_at.desc)
+    @title = 'Blog'
     erb :'blog/index'
   end
   
   get :post, :map => '/blog/:slug' do 
     @blog_post = BlogPost.find_by(slug: params[:slug])
+    @title = @blog_post.title
     erb :'blog/post'
   end  
   
