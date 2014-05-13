4f37ed5cbb0ccb21588d2394c70667bdd9222195
Stephen Reid <postscript07@gmail.com>
2014-05-13 22:58:27 +0100

    .

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'activate-admin', github: 'wordsandwriting/activate-admin'
 # Data storage
 gem 'mongoid', github: 'mongoid/mongoid', ref: 'e1b32e598ec231cc7a7e191fd0432e4cd4910447'
 gem 'dragonfly'
+gem 'dragonfly-s3_data_store'
 
 # Authentication
 gem 'bcrypt'
@@ -23,15 +24,6 @@ gem 'omniauth-linkedin'
 
 # Error reporting
 gem 'airbrake'
-
-group :production do  
-  gem 'dragonfly-s3_data_store'   
-end
-
-group :development, :test do
-  gem 'dragonfly-mongo_data_store'
-  gem 'bson_ext'
-end
 
 group :test do
   gem 'factory_girl'
--- lib/dragonfly.rb
+++ lib/dragonfly.rb
@@ -1,12 +1,19 @@
 Dragonfly.app.configure do    
   plugin :imagemagick
-  url_format '/media/:job/:name'
-    
-  case Padrino.env
-  when :development, :test
-    datastore :mongo    
-  when :production
-    datastore :s3, {:bucket_name => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['S3_ACCESS_KEY'], :secret_access_key => ENV['S3_SECRET']}
-  end  
-   
+  url_format '/media/:job/:name'    
+  datastore :s3, {:bucket_name => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['S3_ACCESS_KEY'], :secret_access_key => ENV['S3_SECRET']}   
+  
+  define_url do |app, job, opts|    
+    if thumb = Thumb.find_by(signature: job.signature)
+      app.datastore.url_for(thumb.uid)
+    else
+      app.server.url_for(job)
+    end
+  end
+
+  before_serve do |job, env|
+    uid = job.store
+    Thumb.create!(uid: uid, signature: job.signature)
+  end
+  
 end
