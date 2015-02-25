1fb2efdbc0228e79db332674a70aa017c292f2e7
Stephen Reid <postscript07@gmail.com>
2015-02-25 13:26:05 +0000

    use puma

--- config/boot.rb
+++ config/boot.rb
@@ -7,6 +7,8 @@ require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, RACK_ENV)
 
+Rack::Timeout.timeout = 25
+
 Padrino.load!
 
 Mongoid.load!("#{PADRINO_ROOT}/config/mongoid.yml")
