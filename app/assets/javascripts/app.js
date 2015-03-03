1aab42fa26f5ea816e62e3800657bba320e90f41
Stephen Reid <postscript07@gmail.com>
2015-03-03 18:50:55 +0000

    .

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,8 @@
-$(function() {
+$(function () {
 
-  $(document).on('click', 'a[data-confirm]', function(e) {
+  webshims.polyfill();
+
+  $(document).on('click', 'a[data-confirm]', function (e) {
     var message = $(this).data('confirm');
     if (!confirm(message)) {
       e.preventDefault();
@@ -8,11 +10,9 @@ $(function() {
     }
   });
 
-  $(document).on('click', 'a.popup', function(e) {
+  $(document).on('click', 'a.popup', function (e) {
     window.open(this.href, null, 'scrollbars=yes,width=600,height=600,left=150,top=150').focus();
     return false;
   });
-  
-  // $("body").fitVids();
 
 });
