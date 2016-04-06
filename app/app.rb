a10d4436929fa82d8e7a397570326084e1a6fdc8
postscript07@gmail.com <postscript07@gmail.com>
2016-04-06 19:25:33 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -25,10 +25,13 @@ module ActivateApp
     
     Mail.defaults do
       delivery_method :smtp, {
-        :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-        :password => ENV['MAILGUN_SMTP_PASSWORD'],
-        :address => ENV['MAILGUN_SMTP_SERVER'],
-        :port => ENV['MAILGUN_SMTP_PORT']
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :domain => 'heroku.com',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
       }   
     end 
        
