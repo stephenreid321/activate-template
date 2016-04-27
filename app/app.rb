4210f2f06f9b372623485048c85e06172cdc0198
postscript07@gmail.com <postscript07@gmail.com>
2016-04-27 11:59:11 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -39,6 +39,7 @@ module ActivateApp
       redirect "http://#{ENV['DOMAIN']}#{request.path}" if ENV['DOMAIN'] and request.env['HTTP_HOST'] != ENV['DOMAIN']
       Time.zone = current_account.time_zone if current_account and current_account.time_zone    
       fix_params!
+      @_params = params; def params; @_params; end # force controllers to inherit the fixed params
     end        
                 
     error do
